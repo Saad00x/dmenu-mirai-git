c15e46760d37eadba39a97ed97f5fd2081947a31
Saad <76122563+Saad00x@users.noreply.github.com>
2021-09-16 04:03:29 +0100

    Update config.h

--- config.h
+++ config.h
@@ -10,13 +10,13 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#81A0BF" },
+	[SchemeSel] = { "#eeeeee", "#928374" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 29;
+static unsigned int lineheight = 25;
 static unsigned int min_lineheight = 8;
 
 /*
